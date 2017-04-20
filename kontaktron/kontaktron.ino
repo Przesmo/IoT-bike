751234cbb88790d97b04b64f8d423da52bdca15e
Oksymar <mkmmiichchaall@gmail.com>
2017-04-20 19:42:16 +0200

    2 kontarktrony z korby gotowe(probably)

--- kontaktron/kontaktron.ino
+++ kontaktron/kontaktron.ino
@@ -1,46 +1,79 @@
-volatile int steps;
-volatile int oldSteps;
-volatile unsigned int set1Second;
-volatile unsigned int speeed;
-static unsigned long lastTime2;
+
+// kontraktor 1 - korba
+volatile int steps_kon1;
+static unsigned long lastTime2_kon1;
+volatile unsigned int set1Second_kon1;
+static unsigned long lastTime_kon1;
+
+// kontraktor 2 - korba
+volatile int steps_kon2;
+static unsigned long lastTime2_kon2;
+volatile unsigned int set1Second_kon2;
+static unsigned long lastTime_kon2;
+
 unsigned int timer;
-static unsigned long lastTime;
+
+
 void setup()
 {
   Serial.begin(9600);
   pinMode(2, INPUT_PULLUP);
-  pinMode(3,INPUT);
-  attachInterrupt(digitalPinToInterrupt(2), onStep,RISING);
-  attachInterrupt(digitalPinToInterrup(3),onStep,RISING);
+  pinMode(3, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(2), onStep_1, RISING);   // kontraktor 1 - korba
+  attachInterrupt(digitalPinToInterrupt(3), onStep_2, RISING);   // kontraktor 2 - korba
   timer = millis();
-  //dodac dwa kontakrony pin 2,5,7
 }
 
 void loop()
 {
   delay(10);
- 
- if(millis()>timer){
-  timer = millis()+1000;
-  Serial.print("Steps: ");
-  Serial.println(steps);
-  Serial.print("Time: ");
-  Serial.println(set1Second);
-  Serial.println((steps*1000)/set1Second)); //przeliczyc na km/h
- // lastTime =millis();
-  steps = 0;
-  set1Second = 0; 
+
+  if(millis()>timer){
+    timer = millis()+1000;
+
+    // kontaktron 1
+    Serial.print("Steps1: ");
+    Serial.println(steps_kon1);
+    Serial.print("Time1: ");
+    Serial.println(set1Second_kon1);
+    Serial.print("Velocity: ");
+    Serial.println((((float)steps_kon1*1000)/set1Second_kon1)*3.6);
+
+    // kontaktron 2
+    Serial.print("Steps2: ");
+    Serial.println(steps_kon2);
+    Serial.print("Time2: ");
+    Serial.println(set1Second_kon2);
+    Serial.print("Velocity: ");
+    Serial.println((((float)steps_kon2*1000)/set1Second_kon2)*3.6);
+
+    steps_kon1 = 0;
+    steps_kon2 = 0;
+    set1Second_kon1 = 0;
+    set1Second_kon2 = 0;
  }
 }
 
-void onStep()
+void onStep_1()
 {
-  unsigned long timeNow = millis();
-  lastTime2 = timeNow - lastTime;
- set1Second+=lastTime2;
- if (lastTime2 < 50)
+  unsigned long timeNow_kon1 = millis();
+  lastTime2_kon1 = timeNow_kon1 - lastTime_kon1;
+  set1Second_kon1 += lastTime2_kon1;
+  if (lastTime2_kon1 < 50)
     return;
 
-  steps++;
-  lastTime = timeNow;
+  steps_kon1++;
+  lastTime_kon1 = timeNow_kon1;
+}
+
+void onStep_2()
+{
+  unsigned long timeNow_kon2 = millis();
+  lastTime2_kon2 = timeNow_kon2 - lastTime_kon2;
+  set1Second_kon2 += lastTime2_kon2;
+  if (lastTime2_kon2 < 50)
+    return;
+
+  steps_kon2++;
+  lastTime_kon2 = timeNow_kon2;
 }
