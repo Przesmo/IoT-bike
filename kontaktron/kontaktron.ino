86c391531e0a22cb4458aac4e871e97731838c76
Oksymar <mkmmiichchaall@gmail.com>
2017-04-21 02:52:40 +0200

    kod dla 3 kontaktronow

--- kontaktron/kontaktron.ino
+++ kontaktron/kontaktron.ino
@@ -11,6 +11,12 @@ static unsigned long lastTime2_kon2;
 volatile unsigned int set1Second_kon2;
 static unsigned long lastTime_kon2;
 
+// kontraktor 3 - kolo
+volatile int steps_kon3;
+static unsigned long lastTime2_kon3;
+volatile unsigned int set1Second_kon3;
+static unsigned long lastTime_kon3;
+
 unsigned int timer;
 
 
@@ -19,8 +25,10 @@ void setup()
   Serial.begin(9600);
   pinMode(2, INPUT_PULLUP);
   pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(2), onStep_1, RISING);   // kontraktor 1 - korba
   attachInterrupt(digitalPinToInterrupt(3), onStep_2, RISING);   // kontraktor 2 - korba
+  attachInterrupt(digitalPinToInterrupt(4), onStep_3, RISING);   // kontraktor 3 - kolo
   timer = millis();
 }
 
@@ -37,7 +45,7 @@ void loop()
     Serial.print("Time1: ");
     Serial.println(set1Second_kon1);
     Serial.print("Velocity: ");
-    Serial.println((((float)steps_kon1*1000)/set1Second_kon1)*3.6);
+    Serial.println(((steps_kon1*1000*10000)/set1Second_kon1)*3.6);  // nie dziala poprawnie bo liczy jak na int'ach
 
     // kontaktron 2
     Serial.print("Steps2: ");
@@ -45,13 +53,22 @@ void loop()
     Serial.print("Time2: ");
     Serial.println(set1Second_kon2);
     Serial.print("Velocity: ");
-    Serial.println((((float)steps_kon2*1000)/set1Second_kon2)*3.6);
+    Serial.println(((steps_kon2*1000*10000)/set1Second_kon2)*3.6);  // nie dziala poprawnie bo liczy jak na int'ach
+
+    Serial.print("Steps3: ");
+    Serial.println(steps_kon3);
+    Serial.print("Time3: ");
+    Serial.println(set1Second_kon3);
+    Serial.print("Velocity: ");
+    Serial.println(((steps_kon3*1000*10000)/set1Second_kon3)*3.6);  // nie dziala poprawnie bo liczy jak na int'ach
 
     steps_kon1 = 0;
     steps_kon2 = 0;
+    steps_kon3 = 0;
     set1Second_kon1 = 0;
     set1Second_kon2 = 0;
- }
+    set1Second_kon3 = 0;
+  }
 }
 
 void onStep_1()
@@ -76,4 +93,16 @@ void onStep_2()
 
   steps_kon2++;
   lastTime_kon2 = timeNow_kon2;
+}
+
+void onStep_3()
+{
+  unsigned long timeNow_kon3 = millis();
+  lastTime2_kon3 = timeNow_kon3 - lastTime_kon3;
+  set1Second_kon3 += lastTime2_kon3;
+  if (lastTime2_kon3 < 50)
+    return;
+
+  steps_kon3++;
+  lastTime_kon3 = timeNow_kon3;
 }
